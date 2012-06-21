05ca5b0c84260b208a72c1bc508160a481f5f39d
Andras Lasso <lasso@cs.queensu.ca>
2012-06-21 08:59:03 -0400

    Switched to official dcmtk.rt repository

--- CMakeExternals/DCMTK.cmake
+++ CMakeExternals/DCMTK.cmake
@@ -23,7 +23,8 @@ if(${add_project})
   if(CTK_SUPERBUILD)
 
     if(NOT DEFINED DCMTK_DIR)
-      set(revision_tag ae3b946f6e6231)
+      #set(revision_tag ae3b946f6e6231) # without DCMRT
+      set(revision_tag f2b71c92461085718f3bccd1c7415382d3e11e3a) # with DCMRT
       if(${proj}_REVISION_TAG)
         set(revision_tag ${${proj}_REVISION_TAG})
       endif()
@@ -35,7 +36,8 @@ if(${add_project})
         set(location_args GIT_REPOSITORY ${${proj}_GIT_REPOSITORY}
                           GIT_TAG ${revision_tag})
       else()
-        set(location_args GIT_REPOSITORY "${git_protocol}://git.dcmtk.org/dcmtk.git"
+        #set(location_args GIT_REPOSITORY "${git_protocol}://git.dcmtk.org/dcmtk.git" # without DCMRT
+        set(location_args GIT_REPOSITORY "http://git.dcmtk.org/dcmtk.git.rt" #  with DCMRT
                           GIT_TAG ${revision_tag})
       endif()
 
